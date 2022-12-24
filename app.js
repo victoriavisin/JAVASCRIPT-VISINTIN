74296dd55d5327163aa1dcf70f9c15e5e6c70431
victoria.visin <vvisintin18@gmail.com>
2022-12-24 11:29:54 -0300

    Empiezo a darle funcionalidad al carrito de compras

--- app.js
+++ app.js
@@ -60,6 +60,10 @@
 
 
 
+
+
+
+
 // //  <==================AGREGO NUEVOS PRODUCTOS/ARRAY/MAP=========================>
 
 // const nuevosProductos = [
@@ -159,82 +163,266 @@
 
 
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////JAVASCRIPT FINAL//////////////////////////////////////////////////////////////
+
+//Variables
+// const verCarrito = document.querySelector ("#btn-carrito")
+// const modalCointainer = document.querySelector ("#modal-container")
+
+
+//  //Evento
+// const productos = document.querySelectorAll(".card");
+//  productos.forEach((card) => {
+//   card.addEventListener("click", (e) => {
+//      leerProducto(e.target.parentElement);
+//   });
+//  });
+
+//  //Array vacio para guardar los productos
+// let productosCarrito = [];
+
+// function leerProducto(producto) {
+// const infoProducto = {
+//      titulo: producto.querySelector(".card-title").textContent,
+//      precio: producto.querySelector(".card-text").textContent,
+//    };
+
+//    //Agrega elementos al carrito
+//   productosCarrito = [...productosCarrito, infoProducto];
+
+//   carritoHTML();
+//  }
 
 
 
+//  const carrito = document.querySelector(".carrito");
 
+//  function carritoHTML() {
+//    //Limpiar el HTML
+//   limpiarHTML();
 
-
-
-
-const productos = document.querySelectorAll(".card");
-productos.forEach((card) => {
-  card.addEventListener("click", (e) => {
-    leerProducto(e.target.parentElement);
-  });
-});
-
-//Array vacio para guardar los productos
-let productosCarrito = [];
-
-function leerProducto(producto) {
-  const infoProducto = {
-    titulo: producto.querySelector(".card-title").textContent,
-    precio: producto.querySelector(".card-text").textContent,
-  };
-
-  //Agrega elementos al carrito
-  productosCarrito = [...productosCarrito, infoProducto];
-  console.log(productosCarrito);
-
-  carritoHTML();
-}
-
-
-
-const carrito = document.querySelector(".carrito");
-
-function carritoHTML() {
-  //Limpiar el HTML
-  limpiarHTML();
-
-  productosCarrito.forEach((producto) => {
-    const row = document.createElement("div");
-    row.innerHTML = `
-    <div class="container">
-    <h5>${producto.titulo}</h5>
-    <p>${producto.precio}</p>
-    <button class="btn btn-danger">Eliminar</button>
-    </div>
+//    productosCarrito.forEach((producto) => {
+//      const row = document.createElement("div");
+//      row.innerHTML = `
+//      <div class="container">
+//      <h5>${producto.titulo}</h5>
+//      <p>${producto.precio}</p>
+//      <button class="btn btn-danger">Eliminar</button>
+//      </div>
             
-    `;
-    carrito.appendChild(row);
-  });
+//      `;
+//      carrito.appendChild(row);
+//    });
+//  }
+
+
+/////////////////////////////////////////
+
+
+
+
+
+///////////////////////////////////////
+
+
+// function limpiarHTML() {
+//   carrito.innerHTML = "";
+//  }
+
+
+     
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// // VARIABLES
+
+// const contenido = document.querySelector (".card")
+// const verCarrito = document.querySelector ("#btn-carrito")
+// const modalCointainer = document.querySelector ("#modal-container")
+
+
+// // ARRAY 
+
+// let carrito = [];
+
+// // EVENTO
+
+// const productos = document.querySelectorAll(".card");
+//   productos.forEach((card) => {
+//   card.addEventListener("click", (e) => {
+//      leerProducto(e.target.parentElement);
+//   });
+// });
+
+// // FUNCIONES
+
+
+  
+
+// function leerProducto(producto) {
+//   const infoProducto = {
+//    titulo: producto.querySelector(".card-title").textContent,
+//    precio: producto.querySelector(".card-text").textContent,
+//    };
+  
+//   //Agregar elementos al carrito
+
+//    productosCarrito = [...productosCarrito, infoProducto];
+  
+//    carritoHTML(); // QUE FUNCION TIENE ?
+//    }
+
+
+// // MODAL
+
+//    verCarrito.addEventListener ("click", () => {
+//     const modalHeader = document.createElement ("div");
+//     modalHeader.className = "modal-header"
+//     modalHeader.innerHTML = ` 
+//     <h1 class= "modal-header-title">MI CARRITO</h1>
+//     `;
+//     modalCointainer.appendChild (modalHeader)
+
+//     const modalBoton = document.createElement ("p")
+//     modalBoton.className = "modal-header-boton"
+//     modalBoton.innerText = "x" ;
+
+//     modalHeader.appendChild (modalBoton)
+    
+
+
+//     productosCarrito.forEach((producto) => {
+//        let contenidoCarrito = document.createElement("div");
+//        contenidoCarrito.className = "modal-carrito"
+//        contenidoCarrito.innerHTML = `
+//           <div class="container">
+//           <h5>${producto.titulo}</h5>
+//           <p>${producto.precio}</p>
+//           <button class="btn btn-danger">Eliminar</button>
+//           </div>           
+//          `;
+
+//          modalCointainer.appendChild (contenidoCarrito)
+
+  
+
+//    });
+
+//   });
+
+
+
+//////////////////////////////////ULTIMO INTENTO/////////////////////////////////////////////////
+
+// VARIABLES 
+
+let todosLosProductos = document.querySelector (".productos");
+let contenedorCarrito = document.querySelector (".offcanvas-body");
+let precioTotal = document.querySelector (".price-total")
+
+let comprar = [];
+let totalCarrito = 0;
+
+
+//FUNCIONES 
+
+loadEventListenrs ();
+
+function loadEventListenrs(){
+   todosLosProductos.addEventListener("click", agregarProducto);
+   contenedorCarrito.addEventListener ("click", eliminarProducto);
+}
+  
+
+function agregarProducto (e) {
+   e.preventDefault();
+   if (e.target.classList.contains("btn-add-cart")) {
+      const seleccionarProducto = e.target.parentElement;
+      leerContenido (seleccionarProducto); 
+      //console.log(e.target.parentElement); 
+   }
+   
 }
 
-
-
-
-
-
-
-function limpiarHTML() {
-  carrito.innerHTML = "";
+function eliminarProducto(e) {
+   if (e.target.classList.contains("btn-add-cart")) {
+      const eliminarId = e.target.getAttribute("data-id");
+      comprar.forEach(value => {
+        if (value.id == eliminarId) {
+         let priceReduce = parseFloat(value.precio) * parseFloat(value.cantidad);
+         totalCarrito = totalCarrito - priceReduce;
+         totalCarrito = totalCarrito.toFixed(2);
+         
+        } 
+      })
+      comprar = comprar.filter (producto => producto.id !== eliminarId);
+   }
+   loadHtml();
 }
 
+function leerContenido (producto) {
+   const infoProducto = {
+      
+      modelo: producto.querySelector(".card-title").textContent,
+      precio: producto.querySelector(".card-text").textContent,
+      id: producto.querySelector (".btn-add-cart").getAttribute ("data-id"),
+      cantidad: 1
+   }
+
+   totalCarrito = parseFloat (totalCarrito) + parseFloat (producto.precio);
+   totalCarrito = totalCarrito.toFixed(2);
 
 
 
+   const exist = comprar.some (producto => producto.id === infoProducto.id);
+   if (exist) {
+      const pro = comprar.map (producto => {
+         if (producto.id === infoProducto.id) {
+            producto.cantidad++;
+            return producto;
+         } else {
+           return producto 
+         }
+      });
+      comprar = [...pro];   
+   } else {
+      comprar = [...comprar, infoProducto]
+   }
+   loadHtml ();
+   //console.log (infoProducto)
+}
 
+function loadHtml () {
+   limpiarHTML ();
+   comprar.forEach(producto => {
+      const {imagen,modelo,precio,id,cantidad} = producto;
+      const row =document.createElement ("div")
+      row.classList.add(".offcanvas");
+      row.innerHTML = `
+       <div class="card h-100">
+                <div class="card-body text-center">
+                  <h5 class="card-title-item">${producto.modelo}</h5>
+                  <p class="card-text-item">${producto.precio}</p>
+                  <h6 class="cantidad-item">Cantidad: ${cantidad}</h6>
+                  <a href="" data-id=" " class="btn-add-cart btn-lg" id="boton-eliminar">ELIMINAR</a>
+                </div>
+              </div>
 
+      `;
+      contenedorCarrito.appendChild (row);
 
+      precioTotal.innerHTML = totalCarrito;
+      
+   });
+}
 
+function limpiarHTML () {
+   contenedorCarrito.innerHTML = " ";
+}
 
-
-
-
-
+///////////////////////FORMULARIO///////////////////////////
 
 
 
---
+++ datos.js
@@ -0,0 +1,45 @@
+ productos = [
+
+    {
+       id: 1,
+       imagen: "https://th.bing.com/th/id/OIP.CY2RSxqxqHgO1qIx2KURSgHaHa?pid=ImgDet&rs=1",
+       modelo: "CAMISETA RUSIA TITULAR 2018",
+       precio : 10.000,
+   },
+
+  {
+       id: 2,
+       imagen: "https://th.bing.com/th/id/R.6644aceff05747b86cc0bf44d84f8054?rik=Crd%2bAk8euAaVdg&pid=ImgRaw&r=0",
+       modelo: "CAMISETA PSG TITULAR 2020",
+       precio : 15.500,
+   },
+
+   {
+       id: 3,
+       imagen: "https://mejores.com/wp-content/uploads/2020/06/mejorescamisetas4.jpg",
+       modelo: "CAMISETA INTER ALTERNATIVA 2021",
+       precio : 9.000,
+   },
+
+   {
+       id: 4,
+       imagen: "https://th.bing.com/th/id/OIP.oFm5HhLbTaL7b9NvsXc6igHaHa?pid=ImgDet&rs=1",
+       modelo: "CAMISETA PORTUGAL TITULAR 2016",
+       precio : 5.000,
+   },
+
+   {
+       id: 5,
+       imagen: "https://www.sportingplus.net/3949-thickbox_default/camiseta-de-futbol-galatasaray-sk-primera-201415-nike.jpg",
+       modelo: "CAMISETA GALATASARAY SPOR",
+       precio : 6.500,
+   },
+
+   {
+        id: 6,
+         imagen: "https://www.sportingplus.net/3378-thickbox_default/camiseta-de-futbol-australia-equipo-nacional-local-2014-15-nike.jpg",
+        modelo: "CAMISETA AUSTRALIA TITULAR 2022",
+         precio : 17.500,
+     },
+ ];
+
